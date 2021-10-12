953c5cefab7c5a709390ce1cf41303820d2fba2e
Ashley Whiting <whi19052@byui.edu>
2021-10-11 21:29:33 -0500

    module2

--- Prove05/controllers/auth.js
+++ Prove05/controllers/auth.js
@@ -18,9 +18,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/signup', {
       path: '/signup',
-      pageTitle: 'Signup'
+      pageTitle: 'Signup',
+      errorMessage: message
     });
   };
   
@@ -66,6 +73,7 @@ exports.postSignup = (req, res, next) => {
     User.findOne({email: email})
     .then(userDoc => {
         if (userDoc) {
+            req.flash('error', 'Email exists already, please choose a different email address.');
             return res.redirect('/signup');
         }
         return bcrypt.hash(password, 12)
